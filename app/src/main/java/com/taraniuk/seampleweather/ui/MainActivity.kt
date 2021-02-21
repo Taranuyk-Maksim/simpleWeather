4051ddb4b70aada32686c9ce45db0e33fc9bd509
Maks <maksim.taranyk@gmail.com>
2021-02-21 16:20:02 +0200

    project set up

--- app/src/main/java/com/taraniuk/seampleweather/ui/MainActivity.kt
+++ app/src/main/java/com/taraniuk/seampleweather/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.taraniuk.seampleweather
+package com.taraniuk.seampleweather.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.taraniuk.seampleweather.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
